b22d27826c8ea202f5b5076ebe4c706ce2fd4d4f
netomatiazi <netoomn@gmail.com>
2021-01-25 11:47:31 -0300

    Creating QuizContainer component externally

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import db from '../db.json'
 import Widget from '../src/components/Widget/index'
 import Footer from '../src/components/Footer/index'
+import QuizContainer from '../src/components/QuizContainer/index'
 import GitHubCorner from '../src/components/GitHubCorner/index'
 import QuizLogo from '../src/components/QuizLogo/index'
 
@@ -11,18 +12,6 @@ flex:1;
 background-size: cover;
 background-position: center;
 `
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
-
 
 export default function Home() {
   return (
